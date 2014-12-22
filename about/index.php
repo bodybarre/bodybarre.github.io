fc29d71613522b5f9b9a0102b4ea6fe7e241e826
Benjamin Dale <ben@ben-dale.co.uk>
2014-12-22 18:41:17 +0000

    Fixed bug in about page

--- about/index.php
+++ about/index.php
@@ -1,7 +1,31 @@
 <?php
 	include '../header.php';
-	include '../navigation.php';
 ?>
+			<nav class="navbar navbar-inverse" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>					
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="../about">About</a></li>
+							<li><a href="../timetable">Timetable</a></li>
+							<li><a href="../parties">Parties</a></li>
+							<li><a href="../instructors">Instructors</a></li>
+							<li><a href="../training">Instructor Training</a></li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Blog</a></li>
+							<li><a href="../contact">Contact</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</nav>
 			
 			<div class="row">
 				<div class="col-md-12">
