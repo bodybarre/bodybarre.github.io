bea04009e8d49d781dabeab9ceb5a5ae99b21b68
Benjamin Dale <ben@ben-dale.co.uk>
2014-12-22 23:33:43 +0000

    Updated styling to give input boxes a pink glow.

--- aerial-hoop/index.php
+++ aerial-hoop/index.php
@@ -3,9 +3,27 @@
 	include '../navigation.php';
 ?>
 			
-			<div class="row">
+			<div class="row margined-row">
 				<div class="col-md-12">
 					<img class="img-responsive" src="../images/hoop-banner.png"/>
+				</div>
+			</div>
+			
+			<div class="row margined-row">
+				<div class="col-md-12">
+					Lorem ipsum
+				</div>
+			</div>
+			
+			<div class="row margined-row">
+				<div class="col-xs-12 col-md-offset-3 col-md-6 interested">
+					<p><b><i>Let us know if you're interested in aerial hoop!</i></b></p>
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="E-mail Address">
+						<span class="input-group-btn">
+							<button class="btn btn-primary" type="button">I'm interested!</button>
+						</span>
+					</div>
 				</div>
 			</div>
 			
