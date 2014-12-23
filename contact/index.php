ee43f5d5df53e1934ac27af274dd4e7fbd2c9a5f
Benjamin Dale <ben@ben-dale.co.uk>
2014-12-23 18:25:14 +0000

    Added banner to contact page. Added google map to contact page.

--- contact/index.php
+++ contact/index.php
@@ -27,12 +27,45 @@
 				</div>
 			</nav>
 		
-			<div class="row">
+			<div class="row margined-row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
+					<img class="img-responsive" src="../images/contact_banner.png"/>
 				</div>
 			</div>
 			
+			<div class="row">
+				<div class="col-xs-12 col-sm-8">
+					<h3 class="pink">Where are we?</h3>
+					<p>
+						On the hippest street in Central Manchester you will find the very luxurious, 
+						boutique styled fitness studio, perfectly placed in the hub of the vibrant and 
+						exciting Northern Quarter.
+					</p>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div id="map"></div>
+				</div>
+			</div>
+			<script src="https://maps.googleapis.com/maps/api/js"></script>
+			<script>
+				function init() {
+					var myLatlng = new google.maps.LatLng(53.484233, -2.236942);
+					var mapOptions = {
+						center: myLatlng,
+						zoom: 17,
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+					}
+					var map = new google.maps.Map(document.getElementById('map'), mapOptions)
+					
+					
+					new google.maps.Marker({
+						 position: myLatlng,
+						 map: map,
+						 title: 'Bodybarre Manchester'
+					});
+				}
+				google.maps.event.addDomListener(window, 'load', init);
+			</script>
 <?php
 	include '../footer.php';
 ?>
