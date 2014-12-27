60dc0a63426f9d0a87103ef12e4024877627de8a
Benjamin Dale <ben@ben-dale.co.uk>
2014-12-27 19:11:16 +0000

    Instructors page finished. Waiting for review.

--- instructors/index.php
+++ instructors/index.php
@@ -28,26 +28,78 @@
 		
 			<div class="row margined-row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
+					<img class="img-responsive" src="../images/instructors_banner.png" alt="Instructors Banner"/>
+				</div>
+			</div>
+			
+			<div class="row margined-row">
+				<div class="col-md-12">
+					<i>
+						<h3 class="center pink">
+							Meet the true force of Bodybarre; a team of the most passionate and inspiring teachers in the North West.
+						</h3>
+					</i>
+					</b>
 				</div>
 			</div>
 			
 			<div class="row margined-row">
 				<div class="col-xs-12 col-sm-4">
 					<div class="thumbnail">
-						<img src="http://lorempixel.com/600/500/" alt="...">
+						<img class="img-responsive" src="../images/kaz.jpg" alt="Karen Chaundy Photo">
 						<div class="caption">
-							<h3 class="center">Karen Chaundy</h3>
-							<p>Here is some text.</p>
+							<h3 class="pink center">Karen Chaundy</h3>
+							<i><h4 class="center gold">The strong one!</h4></i>
+							<p>
+								Karen is the founder and inspiration behind Bodybarre. Karen has immersed herself in the 
+								world of fitness and dance all her life starting with gymnastics at 2 years old. Karen continued 
+								with gymnastics; training in Bulgaria and competing at regional squad level before becoming a 
+								club coach and chief choreographer. A career in professional dance followed and included the highlight of performing as an aerialist 
+								and ground based dancer within the Millennium Dome Central Arena Show in London.
+							</p>
+							<p>
+								Karen has also performed at the Bollywood Film Awards Ceremony, a burlesque dancer on the 
+								ITV's drama series ‘The Royal’, a Pole Dancer on Coronation Street and on Shameless of which 
+								she was appointed to perform, teach and choreograph the shows leading actress.
+								Karen continues to choreograph and perform aerial and ground based Burlesque and Pole 
+								Dance shows for TV, Stage and corporate and celebratory events.
+							</p>
+							<p>
+								Karen has continued to build her knowledge within the fitness industry and formalized this 
+								with qualifications with YMCA gym instructor level 2. She has been selected and qualified to 
+								teach New York City Ballet Workout as well as Nike Rockstar Workouts; involvement in these 
+								different dance styles has inspired her and continue to feed her creativity. Keeping her fitness 
+								knowledge current, Karen is one of the UK's first AntiGravity&reg; Aerial Yoga and AntiGravity&reg; 
+								Aerial Pilates qualified Instructors leading AntiGravity&reg; Fitness classes at Bodybarre.
+								Karen is also a Master Trainer for the XPert Pole Instructor Training programs of which she 
+								conducts across Europe.
+							</p>
+							<p>
+								Her Pole Dancing performance career reached its pinnacle as she successfully represented the 
+								UK at the World Pole Dance Championships. Known for her strong and unique pole dance style 
+								she has captivated audiences worldwide and delivers pole masterclasses across the UK and the 
+								world.
+							</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-12 col-sm-4">
 					<div class="thumbnail">
-						<img src="http://lorempixel.com/600/500/" alt="...">
+						<img class="img-responsive" src="../images/penny.jpg" alt="Penny Howarth Photo">
 						<div class="caption">
-							<h3 class="center">Penny Howarth</h3>
-							<p>Here is some text.</p>
+							<h3 class="pink center">Penny Howarth</h3>
+							<i><h4 class="center gold">The bendy one!</h4></i>
+							<p>
+								Penny competes at a professional level in pole dance competitions and holds many pole titles.
+								Penny has worked in the pole dance industry since 2007 but has been dancing since the age of 
+								4! Penny’s training includes many dance disciplines from ballet to tap. Penny’s favourite 
+								pole moves are Jade, Star Gazer, Embrace and Flying Ballerina.
+							</p>
+							<p>
+								Penny’s aim is for every student to achieve something every lesson, no matter how big or 
+								small. Penny feels students should be able to forget any stresses of everyday life and enjoy the 
+								class, leaving the studio feeling empowered, self confident and happy.
+							</p>
 						</div>
 					</div>
 				</div>
@@ -55,8 +107,24 @@
 					<div class="thumbnail">
 						<img class="img-responsive" src="../images/cassie.jpg" alt="Cassie Bee Photo">
 						<div class="caption">
-							<h3 class="center">Cassie Bee</h3>
-							<p>Here is some text.</p>
+							<h3 class="pink center">Cassie Bee</h3>
+							<i><h4 class="center gold">The sexy one!</h4></i>
+							<p>
+								Cassie brings a touch of super sassy sexy pole fun to Bodybarre, and teaches regular Lap Dance Workshops which always go down a storm!
+							</p>
+							<p>
+								Cassie loves to share her fitness knowledge, and makes each class fun whilst delivering safe,
+								effective sessions with an encouraging nature. She finds it hugely rewarding to watch her 
+								students grow in confidence as their fitness goals become achievable.
+							</p>
+							<p>
+								Cassie understands the importance of being a student herself, and she regularly attends 
+								instructor fitness courses, keeping her fitness knowledge current to ensure she delivers the 
+								best classes for her students.
+							</p>
+							<p>
+								Passionate about fitness, Cassie enjoys working with clients who display the same level of enjoyment and love for fitness that she does.
+							</p>
 						</div>
 					</div>
 				</div>
