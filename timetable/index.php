c42859fb3423ce5da4f70065279865aefbf0e603
Benjamin Dale <ben@ben-dale.co.uk>
2015-01-02 20:41:51 +0000

    Timetable finished.

--- timetable/index.php
+++ timetable/index.php
@@ -176,6 +176,109 @@
 						</tr>
 					</table>
 				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h2 class="gold">Thursday</h2>
+					<table class="table table-condensed table-bordered">
+						<tr>
+							<td colspan="2"><h4>Studio #1</h4></td>
+						</tr>
+						<tr>
+							<td class="pink">17:30 - 18:30</td>
+							<td>Aerial Silks - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">18:30 - 19:30</td>
+							<td>Static Trapeze - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">19:30 - 20:15</td>
+							<td>Aerial Dance - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">20:30 - 21:30</td>
+							<td>Aerial Hoop - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+					</table>
+					<table class="table table-condensed table-bordered">
+						<tr>
+							<td colspan="2"><h4>Studio #2</h4></td>
+						</tr>
+						<tr>
+							<td class="pink">17:30 - 18:30</td>
+							<td>Private Studio Hire<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">18:30 - 19:30</td>
+							<td>Gold Pole Sport - Advanced<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">19:30 - 20:15</td>
+							<td>Splits Stretch -  All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">20:30 - 21:30</td>
+							<td>Private Studio Hire<br/><i>Pre-Book</i></td>
+						</tr>
+					</table>
+				</div>
+			
+				<div class="col-md-4">
+					<h2 class="gold">Friday &amp; Saturday</h2>
+					<table class="table table-condensed table-bordered">
+						<tr>
+							<td class="pink">11:00 - 21:00</td>
+							<td>Private Studio Hire<br/><i>Pre-Book</i></td>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="col-md-4">
+					<h2 class="gold">Sunday</h2>
+					<table class="table table-condensed table-bordered">
+						<tr>
+							<td colspan="2"><h4>Studio #1</h4></td>
+						</tr>
+						<tr>
+							<td class="pink">10:30 - 11:30</td>
+							<td>AntiGravity Yoga Flying Fitness<br/>All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">11:30 - 12:30</td>
+							<td>Aerial Silks - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">13:30 - 15:30</td>
+							<td>Pole &amp; Aerial Jam - All Levels<br/><i>Pre-Book, Non-Instructional</i></td>
+						</tr>
+						<tr>
+							<td class="pink">15:30 - 16:30</td>
+							<td>Workshops - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">16:30 - 17:15</td>
+							<td>Bendy Backs - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">17:30 - 18:30</td>
+							<td>Aerial Hoop - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+					</table>
+					<table class="table table-condensed table-bordered">
+						<tr>
+							<td colspan="2"><h4>Studio #2</h4></td>
+						</tr>
+						<tr>
+							<td class="pink">12:30 - 13:15</td>
+							<td>Kids Aerial Circus - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+						<tr>
+							<td class="pink">14:15 - 15:15</td>
+							<td>Private Tuition - All Levels<br/><i>Pre-Book</i></td>
+						</tr>
+					</table>
+				</div>
 				
 			</div>
 <?php
