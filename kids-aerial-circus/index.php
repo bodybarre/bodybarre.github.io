c828b74ee8e96274db907606580776ab51fd4967
Benjamin Dale <ben@ben-dale.co.uk>
2015-01-02 23:48:23 +0000

    Kids aerial circus page done.

--- kids-aerial-circus/index.php
+++ kids-aerial-circus/index.php
@@ -3,9 +3,64 @@
 	include '../navigation.php';
 ?>
 			
-			<div class="row">
+			<div class="row margined-row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
+					<img class="img-responsive" src="../images/kids-circus_banner.png"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Four Week Course - All levels, ages 4 to 15</h3>
+					<p>
+						Kids Aerial Circus classes are a great way for kids to get fit, meet new people and 
+						learn an interesting range of skills. Our classes are well-structured, with a clear 
+						progression throughout each term, we teach the kids how to become budding circus 
+						stars in a safe and fun environment. The weekly sessions target a number of 
+						different skills throughout the term. We use specialized equipment including a 
+						matted floor area, gymnastic crash mats, training bar, and a floor balance beam as 
+						well as Circus apparatus including Vertical Pole, Silks, Static Trapeze and Aerial Hoop.
+					</p>
+					<p>
+						Safety based and technique driven class instruction covers everything from fitness to 
+						performance. We combine the core elements of basic gymnastic skills with fun circus 
+						apparatus to make exercise more active and exciting. No prior experience is required 
+						to join the fun. Children are encouraged to strive for, and reach individual goals,
+						paced to their own ability, all within a non-competitive environment.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">Exercise Benefits</h3>
+							<ul class="ticked-list">
+								<li><span>Increase flexibility and strength</span></li>
+								<li><span>Improve physical, mental and emotional health</span></li>
+								<li><span>Better self-discipline</span></li>
+								<li><span>Boost self confidence</span></li>
+								<li><span>Promote social skills</span></li>
+								<li><span>Sharpen motor skills and muscle memory</span></li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">What to wear?</h3>
+							<p>
+								Shorts, jogging bottoms or leggings. Tight fitting clothing such as a t-shirt so clothing does not ride up.
+							</p>
+						</div>
+					</div>					
+				</div>
+			</div>
+			<div class="row margined-row">
+				<div class="col-md-6">
+					<h3 class="pink">How to book?</h3>
+					<p>Book in advance only; essential to secure your place.</p>
+					<p>To check availability and to book a place, contact: <span class="gold">karen@bodybarre.co.uk</span></p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">How much?</h3>
+					<p>This four week course costs £30.00</p>
 				</div>
 			</div>
 			
