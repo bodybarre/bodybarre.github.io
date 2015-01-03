a4bf186f10285b17302fcbd8e714ff29bc5d075b
Benjamin Dale <ben@ben-dale.co.uk>
2015-01-03 00:29:33 +0000

    Finished burlesque page, updated kids circus page. Created and added burlesque banner.

--- burlesque/index.php
+++ burlesque/index.php
@@ -5,7 +5,79 @@
 			
 			<div class="row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
+					<img class="img-responsive" src="../images/burlesque_banner.png" alt="Burlesque Banner"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Four Week Course - All levels and suitable for the absolute beginner</h3>
+					<p>
+						Want to flirt with feathers, twirl tassels, peel gloves, and all that goes with a burlesque pop?
+						Learn an easy to follow, fun Burlesque dance routine using props from our dressing up box. 
+						Techniques will cover; using your props sensually, facial expressions, erotic body language, floor 
+						dance and of course enjoying the delight of a brilliant burlesque routine. 
+					</p>
+					<p>
+						Be sexy, move to the music, have fun and if all fails just wiggle! Prepare to dress up and dress down.
+					</p>
+					<h3 class="pink">Professional burlesque dance troupe</h3>
+					<p>
+						Bodybarre produces full length and short version shows to bring a unique Burlesque buzz for any 
+						occasion.
+						Book the Bodybarre Burlesquers to entertain at your event and they will arrive armed and ready with 
+						twirling tassels, colourful costumes and sleek sensual dance routines.
+						Our dancers have appeared on TV, entertained at corporate events, and even danced the Can-Can on 
+						ice on the TV show "Torvill and Dean's Dancing On Ice"!
+					</p>
+					<p>
+						Fun loving dance packages are available for high-class tease and engaging excitement.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">Exercise Benefits</h3>
+							<ul class="ticked-list">
+								<li><span>Increase flexibility</span></li>
+								<li><span>Strengthen core</span></li>
+								<li><span>Firm your bum, abs, legs and arms</span></li>
+								<li><span>Feel good and energized</span></li>
+								<li><span>Improve balance and posture</span></li>
+								<li><span>Reduce stress levels</span></span></li>
+								<li><span>Strengthen heart with low cardio impact</span></span></li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">What to wear?</h3>
+							<p>
+								High Heeled shoes for Burlesque Dance. Please ensure your shoes have a strap, no sling backs.
+								Frilly knickers or leggings, corset or t-shirt, please wear whatever you feel comfortable in.
+								Bodybarre provide fun dress-up props such as hats, boas, gloves and fans.
+								Students may be required to supply their own props such as stockings, shirt or dress to use in class – nothing is required for week one of the course, 
+								your instructor will inform you if an item is required.
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+							<p><b>Tuesday</b>: <span class="gold">18:30 - 19:15</span></p>
+						</div>
+					</div>		
+				</div>
+			</div>
+			<div class="row margined-row">
+				<div class="col-md-6">
+					<h3 class="pink">How to book?</h3>
+					<p>Book in advance only; limited places due to limited props.</p>
+					<p>To check availability and to book a place, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">How much?</h3>
+					<p>The four week burlesque course costs £30.00.</p>
+					<p>Prices vary for burlesque show. E-mail <span class="gold">karen@bodybarre.co.uk</span> to discuss your requirements.</p>
 				</div>
 			</div>
 			
--- kids-aerial-circus/index.php
+++ kids-aerial-circus/index.php
@@ -5,7 +5,7 @@
 			
 			<div class="row margined-row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="../images/kids-circus_banner.png"/>
+					<img class="img-responsive" src="../images/kids-circus_banner.png" alt="Kids Aerial Circus Banner"/>
 				</div>
 			</div>
 			<div class="row">
@@ -15,7 +15,10 @@
 						Kids Aerial Circus classes are a great way for kids to get fit, meet new people and 
 						learn an interesting range of skills. Our classes are well-structured, with a clear 
 						progression throughout each term, we teach the kids how to become budding circus 
-						stars in a safe and fun environment. The weekly sessions target a number of 
+						stars in a safe and fun environment.
+					</p>
+					<p>
+						The weekly sessions target a number of 
 						different skills throughout the term. We use specialized equipment including a 
 						matted floor area, gymnastic crash mats, training bar, and a floor balance beam as 
 						well as Circus apparatus including Vertical Pole, Silks, Static Trapeze and Aerial Hoop.
@@ -49,7 +52,13 @@
 								Shorts, jogging bottoms or leggings. Tight fitting clothing such as a t-shirt so clothing does not ride up.
 							</p>
 						</div>
-					</div>					
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+							<p><b>Sunday</b>: <span class="gold">12:30 - 13:15</span></p>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="row margined-row">
