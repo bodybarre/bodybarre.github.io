188a8472ce329543a676e21cae5707371cc7ba85
Benjamin Dale <ben@ben-dale.co.uk>
2015-01-03 12:51:48 +0000

    Finished kettlercise page. Added banner.

--- kettlercise/index.php
+++ kettlercise/index.php
@@ -5,7 +5,74 @@
 			
 			<div class="row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
+					<img class="img-responsive" src="../images/kettlercise_banner.png" alt="Kettlercise banner"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Kettlebell Training - All levels and suitable for the absolute beginner</h3>
+					<p>
+						Get sweating and release the ‘happy’ chemicals, get a fitter ticker, tone up, burn fat, and enjoy a healthier you.
+					</p>
+					<p>
+						Kettlercise is a great way for you to reduce body fat while learning some many basic Kettlebell 
+						exercises. Whether you are a total novice or a regular kettlebell user, this class suits all 
+						abilities. Kettlercise uses a whole body approach, targeting each muscle group for a balanced 
+						workout. 
+					</p>
+					<p>
+						Mixing resistance training with cardiovascular training helps to sculpt the body very 
+						quickly especially when coupled with a healthy balanced diet.
+					</p>
+					<p>
+						Kettlercise will not bulk up the 
+						body, the system is designed to deliver cardio fat burning fitness which keeps the body lean 
+						and sculpted.
+					</p>
+					<p class="gold">
+						<i><b>Slash calories and reap major health benefits!</b></i>
+					</p>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">Exercise Benefits</h3>
+							<ul class="ticked-list">
+								<li><span>Full body workout</span></li>
+								<li><span>Develop strength in core and back</span></li>
+								<li><span>Tone legs, bum, hips and abs</span></li>
+								<li><span>Burn 650 calories (approx.) in a 45 minute class</span></li>
+								<li><span>Improve your cardiovascular health</span></li>
+								<li><span>Lose fat</span></li>
+								<li><span>Prevent injury</span></li>
+								<li><span>Improve posture</li></span>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">What to wear?</h3>
+							<p>
+								Wear regular fitness clothes that you can move in and trainers. Sweat wristbands (optional). Bring a towel.
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+							<p><b>Monday</b>: <span class="gold">19:00 - 19:45</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row margined-row">
+				<div class="col-md-6">
+					<h3 class="pink">How to book?</h3>
+					<p>No need to book! This is a weekly drop in class; just turn up!</p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">How much?</h3>
+					<p>£6 per class.</p>
 				</div>
 			</div>
 			
