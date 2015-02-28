9c5ce779893f42d58316b976109a3352cc42d246
ben-dale <ben@ben-dale.co.uk>
2015-02-28 21:11:04 +0000

    finished aerial-hoop page

--- aerial-hoop/index.php
+++ aerial-hoop/index.php
@@ -8,22 +8,60 @@
 					<img class="img-responsive" src="../images/hoop-banner.png"/>
 				</div>
 			</div>
-			
-			<div class="row margined-row">
-				<div class="col-md-12">
-					Lorem ipsum
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Six Week Course - All levels and suitable for the absolute beginner</h3>
+					<p>
+						Aerial hoop is an amazing apparatus, which can be used in static or spinning mode.
+					</p>
+					<p>
+						Learn how to activate your core and build the strength you’ll need to get up and into 
+						the hoop! This course teaches you a mix of spins and poses with a big strength 
+						component for your upper body and abs, plus all over body flexibility and 
+						coordination.
+					</p>
+					<p>
+						You will develop aerial sequences moving towards holding yourself up for minutes at a time!
+					</p>
+					<p class="gold">
+						<i><b>Feel like a circus superstar!</b></i>
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">Exercise Benefits</h3>
+					<ul class="ticked-list">
+						<li><span>Increased muscle tone</span></li>
+						<li><span>Increased strength, particularly in the arms and back</span></li>
+						<li><span>Decreased body fat</span></li>
+						<li><span>Better cardiovascular fitness</span></li>
+						<li><span>Increased flexibility</span></li>
+						<li><span>Increased spatial awareness and co-ordination</span></li>
+						<li><span>Confidence boosting</span></li>
+					</ul>
 				</div>
 			</div>
-			
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">What to wear?</h3>
+					<p>
+						Tight fitting clothing such as a t-shirt and leggings are recommended for safety and comfort, so clothing does not ride up.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">When?</h3>
+					<p><b>Thursday</b>: <span class="gold">20:30 - 21:30</span></p>
+					<p><b>Sunday</b>: <span class="gold">17:30 - 18:30</span></p>
+				</div>
+			</div>
 			<div class="row margined-row">
-				<div class="col-xs-12 col-md-offset-3 col-md-6 interested">
-					<p><b><i>Let us know if you're interested in aerial hoop!</i></b></p>
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="E-mail Address">
-						<span class="input-group-btn">
-							<button class="btn btn-primary" type="button">I'm interested!</button>
-						</span>
-					</div>
+				<div class="col-md-6">
+					<h3 class="pink">How to book?</h3>
+					<p>Book in advance only; essential to secure your place.</p>
+					<p>To check availability and to book a place, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">How much?</h3>
+					<p>This six week course costs £55.00</p>
 				</div>
 			</div>
 			
