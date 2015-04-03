c481dec8e1ca66bc5d65e8fe1f9fd76464ce3eb8
ben-dale <ben@ben-dale.co.uk>
2015-04-03 15:13:12 +0100

    Added twerking page and banner image

--- twerking/index.php
+++ twerking/index.php
@@ -2,13 +2,64 @@
 	include '../header.php';
 	include '../navigation.php';
 ?>
-			
-			<div class="row">
-				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
-				</div>
+
+		<div class="row margined-row">
+			<div class="col-md-12">
+				<img class="img-responsive" src="../images/twerking_banner.png"/>
 			</div>
-			
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<h3 class="pink">Twerking Team - All levels and suitable for the absolute beginner</h3>
+				<p>
+					Thanks to Miley Cyrus we now all know exactly what Twerking is!
+				</p>
+				<p>
+					Twerking is a high-intensity cardio class that feels more like a dance club outing than a
+					gym session. We have taken the uncontrollable desire to shake our ass and mixed it with
+					some well known bum toning exercises aka squats!
+					TWERK off the calories with a rhythmic shaking of one’s behind.
+					The most fun conditioning class by far!
+				</p>
+				<p class="gold">
+					<i><b>Join the Twerking fitness craze!</b></i>
+				</p>
+			</div>
+			<div class="col-md-6">
+				<h3 class="pink">Exercise Benefits</h3>
+				<ul class="ticked-list">
+					<li><span>Full body workout</span></li>
+					<li><span>Strengthen your bum, thighs, hips, lower back and abs</span></li>
+					<li><span>Increase your cardiovascular endurance</span></li>
+					<li><span>Better cardiovascular fitness</span></li>
+					<li><span>Burn approximately 8 calories a minute!</span></li>
+					<li><span>Shape and strengthen your core muscles</span></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<h3 class="pink">What to wear?</h3>
+				<p>
+					Regular fitness clothes that you can move in, layers to keep the body warm, and layers you can strip when warm! Wear trainers for an extra heel bounce!
+				</p>
+			</div>
+			<div class="col-md-6">
+				<h3 class="pink">When?</h3>
+				<p><b>Monday</b>: <span class="gold">18:30 - 19:00</span></p>
+			</div>
+		</div>
+		<div class="row margined-row">
+			<div class="col-md-6">
+				<h3 class="pink">How to book?</h3>
+				<p>No need to book! This is a weekly drop in class; just turn up!</p>
+			</div>
+			<div class="col-md-6">
+				<h3 class="pink">How much?</h3>
+				<p>£5 per class.</p>
+			</div>
+		</div>
+
 <?php
 	include '../footer.php';
 ?>
