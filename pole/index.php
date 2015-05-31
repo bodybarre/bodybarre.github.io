76c5689b06bbc3b078f25a9797f4e4543ace8121
Ben Dale <ben@ben-dale.co.uk>
2015-05-31 21:11:36 +0100

    Updated pole page

--- pole/index.php
+++ pole/index.php
@@ -2,13 +2,120 @@
 	include '../header.php';
 	include '../navigation.php';
 ?>
-			
+
 			<div class="row">
 				<div class="col-md-12">
 					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
 				</div>
 			</div>
-			
+
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Bronze Beginners Pole Course - Suitable for the absolute beginner</h3>
+					<p>NEW to Pole Dancing? Then this is your first step…</p>
+					<p>
+						We gently introduce you to the basic art of pole dance as you experience lifting your body weight through
+						various pole moves.
+					</p>
+					<p>
+						During the three week course we will slowly build on new moves to ensure correct technique is practiced to
+						provide a safe and fitness beneficial session. By week three you will have the confidence to put all you have
+						learned into a full dance routine of which includes poses, climbs and spins.
+					</p>
+					<p class="gold">
+						<i><b>Pole Dancing is suitable for men, women and super heroes!</b></i>
+					</p>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">How much?</h3>
+							<p>This three week introductory course costs £50.00.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+							<p><b>Monday</b>: <span class="gold">18:30 - 19:30</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr/>
+
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Pole Squad - Bronze, Silver, Gold &amp; Platinum</h3>
+					<p>Pole Squad is a follow up, weekly pole class for all who have completed at least, the pole dancing Bronze beginner’s course.</p>
+					<p>
+						This is a relaxed series of lessons that provides time for you to train with the irreplaceable friends you will have
+						made within our unique Bodybarre pole dance school. Bodybarre instructors will encourage you to gradually
+						progress your pole dance skills and meet new challenges along the way.
+					</p>
+					<p>
+						We encourage you to train at the same Pole Squad level for at least three months before deciding to take the next level course.
+					</p>
+					<p class="gold">
+						<i><b>Increase your skills at a pace to suit you until you feel ready to tackle the next level.</b></i>
+					</p>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">How much?</h3>
+							<p>This six week course costs £55.00 for Bronze, Silver and Gold levels.</p>
+							<p>This six week course costs £70.00 for Platinum level.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+							<p><b>Monday</b> (All Levels): <span class="gold">17:30 - 18:30</span></p>
+							<p><b>Monday</b> (All Levels): <span class="gold">19:30 - 20:30</span></p>
+							<p><b>Monday</b> (Super Gold Advanced Plus): <span class="gold">20:30 - 21:30</span></p>
+							<p><b>Tuesday</b> (Bronze Beginner): <span class="gold">19:15 - 20:15</span></p>
+							<p><b>Tuesday</b> (Silver Intermediate): <span class="gold">19:15 - 20:15</span></p>
+							<p><b>Tuesday</b> (Platinum Elite): <span class="gold">20:15 - 21:30</span></p>
+							<p><b>Wednesday</b> (Bronze Beginner): <span class="gold">18:30 - 19:30</span></p>
+							<p><b>Wednesday</b> (Silver Intermediate): <span class="gold">19:15 - 20:15</span></p>
+							<p><b>Thursday</b> (Gold Advanced): <span class="gold">18:30 - 19:30</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr/>
+
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Lyrical Pole Dance - Bronze, Silver, Gold &amp; Platinum</h3>
+					<p>Challenge your cardio fitness and indulge in the smoothness of Pole routines.</p>
+					<p>
+						Lyrical Pole Dance offers a more contemporary dance based pole class, which is easy to follow,
+						fun and fat burning!
+					</p>
+					<p>
+						Enjoy the challenge of creative pole dance and learn how to glide effortlessly around the pole.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">How much?</h3>
+							<p>This six week course costs £50.00.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+							<p><b>Wednesday</b>: <span class="gold">19:30 - 20:30</span></p>
+							<p><b>Sunday</b>: <span class="gold">10:30 - 11:30</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
 <?php
 	include '../footer.php';
 ?>
