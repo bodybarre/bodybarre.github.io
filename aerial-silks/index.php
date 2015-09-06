9d084cef24b3063b68d9b11a469f9bfa2e35738d
Benjamin Dale <ben@ben-dale.co.uk>
2015-09-06 10:24:36 +0100

    Add initial content to aerial silks page

--- aerial-silks/index.php
+++ aerial-silks/index.php
@@ -3,9 +3,62 @@
 	include '../navigation.php';
 ?>
 			
-			<div class="row">
+			<div class="hidden-xs row margined-row">
 				<div class="col-md-12">
-					<img class="img-responsive" src="http://lorempixel.com/1200/200/"/>
+					
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">Six Week Course - All levels and suitable for the absolute beginner</h3>
+					<p>
+						Aerial Silks is one of the most inspiring forms of aerial dance. Aerial silk artists climb,
+						twist, spin, and contort themselves on fabric sheets that hang from floor to ceiling.
+					</p>
+					<p>
+						It’s all about the basics in this one hour introduction to aerial silk. 
+						Work at your own pace as we learn climbs, inverts, locks, and slides. 
+						The most possible fun a person can have while enduring a full-body workout! 
+					</p>
+					<p class="gold">
+						<i><b>Get ready to climb, twist and hang!</b></i>
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">Exercise Benefits</h3>
+					<ul class="ticked-list">
+						<li><span>Burn between 300 and 400 calories per one hour class</span></li>
+						<li><span>Incredible strength and stamina</span></li>
+						<li><span>Become long and lean</span></li>
+						<li><span>Develop strong core</span></li>
+						<li><span>Stretch deeper with little strain</span></li>
+						<li><span>Improve balance</span></li>
+						<li><span>Reinforce body alignment</span></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h3 class="pink">What to wear?</h3>
+					<p>
+						Tight fitting clothing such as a t-shirt and leggings are recommended for safety and comfort, 
+						so clothing does not ride up or get tangled in the silks.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">When?</h3>
+					<?php echo getClassTimesFor("Aerial Silks"); ?>
+				</div>
+			</div>
+			<div class="row margined-row">
+				<div class="col-md-6">
+					<h3 class="pink">How to book?</h3>
+					<p>Book in advance only; essential to secure your place.</p>
+					<p>To check availability and to book a place, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">How much?</h3>
+					<p>This six week course costs £55.00.</p>
 				</div>
 			</div>
 			
