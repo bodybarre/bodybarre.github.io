517c7f8bc9b53881b0df4c7adb166e8a9f5f2aa6
Benjamin Dale <ben@ben-dale.co.uk>
2016-08-10 20:20:53 +0100

    initial party page content added

--- parties/index.php
+++ parties/index.php
@@ -1,7 +1,7 @@
 <?php
 	include '../header.php';
 ?>
-			
+
 			<nav class="navbar navbar-inverse" role="navigation">
 				<div class="container-fluid">
 					<div class="navbar-header">
@@ -10,7 +10,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
-						</button>					
+						</button>
 					</div>
 					<div id="navbar" class="navbar-collapse collapse">
 						<ul class="nav navbar-nav">
@@ -24,20 +24,96 @@
 							<li><a href="../blog">Blog</a></li>
 							<li><a href="../contact">Contact</a></li>
 						</ul>
-					</div><!--/.nav-collapse -->
+					</div>
 				</div>
 			</nav>
-		
-			<div class="row">
+
+      <div class="row">
 				<div class="col-md-12">
-					<div class="under-construction">
-						This page is currently under construction.
-						<br/><br/>
-						<i><span class="gold">Check back soon!</span></i>
+					<img class="img-responsive" src="../images/pole_banner.png" alt="Pole Dance Banner"/>
+				</div>
+			</div>
+
+			<div class="row">
+
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-12">
+      				<h3 class="pink">Bodybarre Parties</h3>
+      				<p>
+                Our unique party packages are a great opportunity to get your friends and family together!
+                Let us take the hassle out of planning your Hen Party, Birthday party, Girlie-get-together, Celebratory treat, or Night Out...
+              </p>
+              <p>
+                Choose your lesson style from the options below. Can’t make up your mind? Too much to choose from? Then why not have a party mix of two different styles!
+                No experience required as we will show you how.
+              </p>
+      				<p class="gold">
+      					<i><b>Prepare to giggle yourself silly!</b></i>
+      				</p>
+            </div>
+			    </div>
+          <div class="row">
+            <div class="col-md-12">
+              <h3 class="pink">Where?</h3>
+              <p>
+                We host your Bodybarre party at the most beautiful venue "The Bodybarre Studio" situated in the heart of Central Manchester’s
+                hippest nightlife and a stones throw from the infamous Arndale Shopping Centre. 
+              </p>
+              <p>
+                <address>
+                  <strong>Bodybarre Studio</strong></br>
+                  Second Floor</br>
+                  35-37 Thomas Street</br>
+                  Northern Quarter</br>
+                  Manchester</br>
+                  M4 1NA</br>
+                </address>
+              </p>
+              <p>
+                <b class="gold">Note:</b> The majority of Bodybarre parties can also be held in the comfort of your own home or hotel room.  Please contact us with your requirements.
+              </p>
+            </div>
+          </div>
+        </div>
+
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">How much?</h3>
+							<p>
+                Our parties are cheaper than you think!
+              </p>
+              <p>
+                For a Bodybarre Dance party, we require a £50 booking fee (non-refundable)
+                and an additional £20.00 per person payable on the day of your party.
+              </p>
+              <p>
+                If not all of your party wish to participate but just wish to soak up the atmosphere then the spectator fee of £10.00 per person
+                is payable on the day of your party.
+              </p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="pink">When?</h3>
+              <p>
+                Each party package is 2 hours long. Please contact us with your preferred time slot and date. Subject to availability.
+              </p>
+              <p>
+                  <p><b>Friday/Saturday: </b><span class="gold">11am - 1pm</span></p>
+                  <p><b>Friday/Saturday: </b><span class="gold">1pm - 3pm</span></p>
+                  <p><b>Friday/Saturday: </b><span class="gold">3pm - 5pm</span></p>
+                  <p><b>Friday/Saturday: </b><span class="gold">5pm - 7pm</span></p>
+                  <p><b>Friday/Saturday: </b><span class="gold">7pm - 9pm</span></p>
+              </p>
+						</div>
 					</div>
 				</div>
 			</div>
-			
+
+			<hr/>
+
 <?php
 	include '../footer.php';
 ?>
