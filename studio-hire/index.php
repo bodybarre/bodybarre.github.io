21b0f20961fe0094c46aa2eab0540abb66fff66f
Benjamin Dale <ben@ben-dale.co.uk>
2018-05-04 22:10:39 +0100

    updates

--- studio-hire/index.php
+++ studio-hire/index.php
@@ -27,16 +27,34 @@
 			</div>
 		</div>
 	</nav>
+	
 	<div class="row">
-		<div class="col-md-12">
-			<div class="under-construction">
-				This page is currently under construction.
-				<br/><br/>
-				<i><span class="gold">Check back soon!</span></i>
+		<div class="col-md-6">
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="pink">Studio Hire</h3>
+					<p>
+						When our studio is not in use, we are pleased to offer this facility out for hire. Great for rehearsals, castings, photography, filming location or other fitness companies to hire, Personal Training sessions, and more. Conveniently located in Manchester City Centre, with excellent transport links.
+					</p>
+					<p>
+						You are very welcome at our studio, please do get in touch to discuss your requirements and we will check availability and provide prices.
+					</p>
+					<p class="gold">
+						<i><b>Seduce your sole with head to toe wellness</b></i>
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+				<h3 class="pink">Contact Us</h3>
+				<p>To check availability and to book a viewing, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
+			</div>
 			</div>
 		</div>
+		<div class="col-md-6">
+			<img class="img-responsive" src="../images/studio.jpg" alt="Bodybarre Studio">
+		</div>
 	</div>
-
 <?php
 	include '../footer.php';
 ?>
