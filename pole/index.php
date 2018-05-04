bef25fa96ec368d1e20f39765749dd1e7407ccc6
Benjamin Dale <ben@ben-dale.co.uk>
2018-05-04 19:40:30 +0100

    update pole page

--- pole/index.php
+++ pole/index.php
@@ -10,150 +10,90 @@
 			</div>
 
 			<div class="row">
-				<div class="col-md-6">
-					<h3 class="pink">Bronze Beginners Pole Course - Suitable for the absolute beginner</h3>
-					<p>NEW to Pole Dancing? Then this is your first step…</p>
+				<div class="col-md-12">
+					<h3 class="pink">Pole Fitness</h3>
+					<p>NEW to Pole Dancing? Done it before?</p>
 					<p>
-						We gently introduce you to the basic art of pole dance as you experience lifting your body weight through
-						various pole moves.
+						We have classes for the absolute beginner up to World Class level.
+						<ul class="ticked-list">
+							<li><span>Bronze: Beginner</span></li>
+							<li><span>Silver: Intermediate</span></li>
+							<li><span>Gold: Advanced</span></li>
+							<li><span>Platinum: Elite</span></li>
+						</ul>
 					</p>
 					<p>
-						During the three week course we will slowly build on new moves to ensure correct technique is practiced to
-						provide a safe and fitness beneficial session. By week three you will have the confidence to put all you have
-						learned into a full dance routine of which includes poses, climbs and spins.
+						Learn the art of pole dance as you experience lifting your body weight through various pole moves.
+						We run structured courses to allow each participant to build on new moves over the weeks, and ensure correct technique is practiced to provide a safe and fitness beneficial session. You will have the confidence to put all you have learned into a full dance routine of which includes poses, climbs and spins.
 					</p>
 					<p class="gold">
 						<i><b>Pole Dancing is suitable for men, women and super heroes!</b></i>
 					</p>
 				</div>
-				<div class="col-md-6">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">How much?</h3>
-							<p>This three week introductory course costs £60.00.</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">When?</h3>
-							<?php echo getClassTimesFor("Pole Dancing Course"); ?>
-						</div>
-					</div>
-				</div>
 			</div>
 
 			<hr/>
 
 			<div class="row">
-				<div class="col-md-6">
-					<h3 class="pink">Pole Squad</h3>
-					<p>Pole Squad is a follow up, weekly pole class for all who have completed at least, the pole dancing Bronze beginner’s course.</p>
-					<p>
-						This is a relaxed series of lessons that provides time for you to train with the irreplaceable friends you will have
-						made within our unique Bodybarre pole dance school. Bodybarre instructors will encourage you to gradually
-						progress your pole dance skills and meet new challenges along the way.
-					</p>
-					<p>
-						We encourage you to train at the same Pole Squad level for at least three months before deciding to take the next level course.
-					</p>
-					<p class="gold">
-						<i><b>Increase your skills at a pace to suit you until you feel ready to tackle the next level.</b></i>
-					</p>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">How much?</h3>
-							<p>This six week course costs £60.00 for Bronze, Silver and Gold levels.</p>
-							<p>This six week course costs £75.00 for Platinum level.</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">When?</h3>
-							<?php echo getClassTimesWithLevelsFor("Bronze Pole Sport"); ?>
-							<?php echo getClassTimesWithLevelsFor("Silver Pole Sport"); ?>
-							<?php echo getClassTimesWithLevelsFor("Gold Pole Sport"); ?>
-							<?php echo getClassTimesWithLevelsFor("Super Gold Pole Sport"); ?>
-							<?php echo getClassTimesWithLevelsFor("Platinum Pole Sport"); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<hr/>
-
-			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-12">
 					<h3 class="pink">Lyrical Pole Dance</h3>
 					<p>Challenge your cardio fitness and indulge in the smoothness of Pole routines.</p>
 					<p>
 						Lyrical Pole Dance offers a more contemporary dance based pole class, which is easy to follow,
-						fun and fat burning!
+						fun and fat burning! Enjoy the challenge of creative pole dance and learn how to glide effortlessly around the pole.
 					</p>
 					<p>
-						Enjoy the challenge of creative pole dance and learn how to glide effortlessly around the pole.
+						This class is suitable for the Silver intermediate plus level.
 					</p>
 					<p class="gold">
 						<i><b>Dance like nobody is watching.</b></i>
 					</p>
 				</div>
-				<div class="col-md-6">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">How much?</h3>
-							<p>This six week course costs £60.00.</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">When?</h3>
-							<?php echo getClassTimesWithLevelsFor("Lyrical Pole Dance"); ?>
-						</div>
-					</div>
-				</div>
 			</div>
 
 			<hr/>
 
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-12">
 					<h3 class="pink">Sexy Pole Dance</h3>
 					<p>
-						Strut this way for a sexy stripper style dance class. Strap on your high heels and prepare to steam up the mirrors.
+					Learn a fun routine of which oozes sex appeal, slinky floor work and basic pole tricks. A confidence boosting, girl power class with slut drops, body rolls, hair flicks and twerking booty pops!
 					</p>
 					<p>
-						Learn a fun routine of which oozes sex appeal, slinky floor work and basic pole tricks. A confidence
-						boosting, girl power class with slut drops, body rolls, hair flicks and twerking booty pops!
+					This class is suitable for the Bronze beginner plus level.
 					</p>
 					<p class="gold">
 						<i><b>Dance like someone is watching!</b></i>
 					</p>
 				</div>
-				<div class="col-md-6">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">How much?</h3>
-							<p>This four week course costs £35.00.</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">When?</h3>
-							<?php echo getClassTimesWithLevelsFor("Sexy Pole Dance"); ?>
-						</div>
-					</div>
+			</div>
+
+			<hr/>
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="pink">Sexy Pole Tricks</h3>
+					<p>
+						Stand by! You are about to take sexy to a whole new level! Learn some explosive, jaw dropping flips, kips, and sensational hips! Clack the high heeled shoes, perform spins, and poses and throw down like you mean it!
+					</p>
+					<p>
+						Pole combinations up, on, and around the pole with a little floor-work work thrown in too! 
+						This class is suitable for the Silver intermediate plus level. 
+					</p>
+					<p class="gold">
+						<i><b>Dance like a sexy siren!</b></i>
+					</p>
 				</div>
 			</div>
 
 			<hr/>
 
 			<div class="row">
-				<div class="col-md-6">
-					<h3 class="pink">Pole Jam</h3>
+				<div class="col-md-12">
+					<h3 class="pink">Play Jam</h3>
 					<p>Alongside your weekly pole schedule, you can also come and enjoy some extra practice time.</p>
 					<p>
-						Pole Jam is a great opportunity to work on your favourite pole moves or perfect your pole dance routine. This
+						Play Jam is a great opportunity to work on your favourite pole moves or perfect your pole dance routine. This
 						class is an informal, non-instructional practice session.
 					</p>
 					<p>
@@ -164,58 +104,21 @@
 						<i><b>Practice makes perfect!</b></i>
 					</p>
 				</div>
-				<div class="col-md-6">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">How much?</h3>
-							<p>£5.00 per session, no need to book! This is a weekly drop in class; just turn up!</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">When?</h3>
-							<?php echo getClassTimesWithLevelsFor("Pole and Aerial Jam"); ?>
-						</div>
-					</div>
-				</div>
 			</div>
 
 			<hr/>
 
 			<div class="row">
 				<div class="col-md-6">
-					<h3 class="pink">Exercise Benefits</h3>
-					<ul class="ticked-list">
-						<li><span>Burn as many as 400 calories an hour, which is similar to a good gym session</span></li>
-						<li><span>Gain great upper body strength and tone</span></li>
-						<li><span>Strengthen and sculpt your stomach muscles</span></li>
-						<li><span>Increased all over muscle definition in your bottom, arms and thighs</span></li>
-						<li><span>Release the happy hormones making you feel better and more energetic</span></li>
-						<li><span>Increase confidence and improve fitness levels</span></li>
-					</ul>
+				<h3 class="pink">When are these classes?</h3>
+					<p>Please see our <a href="../timetable">timetable</a> for the latest class listings.</p>
 				</div>
 				<div class="col-md-6">
-					<h3 class="pink">What to wear?</h3>
-					<p>
-						Bare feet. Shorts or 3/4 leggings, crop top, vest or t-shirt.
-						Bring warm clothes you can move in for the warm up and conditioning section.
-						High heeled shoes for Sexy Pole Dance. Please ensure your shoes have a strap.
-					</p>
-					<p>
-						Please do not use lotions/creams/fake tan on your body or hands before attending class as this makes the
-						poles slippery for you and everyone else.
-					</p>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-md-12">
-					<h3 class="pink">How to book?</h3>
+				<h3 class="pink">How to book?</h3>
 					<p>Book in advance only; limited places due to limited poles.</p>
 					<p>To check availability and to book a place, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
 				</div>
 			</div>
-
 <?php
 	include '../footer.php';
 ?>
