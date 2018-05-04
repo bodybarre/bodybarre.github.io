2708ad248aad3875be113c41b51334aad6eaea77
Benjamin Dale <ben@ben-dale.co.uk>
2018-05-04 21:01:31 +0100

    changes

--- burlesque/index.php
+++ burlesque/index.php
@@ -8,14 +8,12 @@
 					<img class="img-responsive" src="../images/burlesque_banner.png" alt="Burlesque Banner"/>
 				</div>
 			</div>
+			
 			<div class="row">
-				<div class="col-md-6">
-					<h3 class="pink">Four Week Course - All levels and suitable for the absolute beginner</h3>
+				<div class="col-md-12">
+					<h3 class="pink">Burlesque</h3>
 					<p>
-						Want to flirt with feathers, twirl tassels, peel gloves, and all that goes with a burlesque pop?
-						Learn an easy to follow, fun Burlesque dance routine using props from our dressing up box.
-						Techniques will cover; using your props sensually, facial expressions, erotic body language, floor
-						dance and of course enjoying the delight of a brilliant burlesque routine.
+						Want to flirt with feathers, twirl tassels, peel gloves, and all that goes with a burlesque pop? Learn an easy to follow, fun Burlesque dance routine using props from our dressing up box. Techniques will cover; using your props sensually, facial expressions, erotic body language, floor dance and of course enjoying the delight of a brilliant burlesque routine.
 					</p>
 					<p>
 						Be sexy, move to the music, have fun and if all fails just wiggle! Prepare to dress up and dress down.
@@ -23,86 +21,61 @@
 					<p class="gold">
 						<i><b>Have fun and feel sexy!</b></i>
 					</p>
-					<h3 class="pink">Professional burlesque dance troupe</h3>
+				</div>
+			</div>
+
+			<hr/>
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="pink">Stiletto Fit</h3>
 					<p>
-						Bodybarre produces full length and short version shows to bring a unique Burlesque buzz for any
-						occasion.
-						Book the Bodybarre Burlesquers to entertain at your event and they will arrive armed and ready with
-						twirling tassels, colourful costumes and sleek sensual dance routines.
-						Our dancers have appeared on TV, entertained at corporate events, and even danced the Can-Can on
-						ice on the TV show "Torvill and Dean's Dancing On Ice"!
+					Yanis Marshall anyone? Yes? No? Never mind - we’ve got it covered! Take to the dance floor with some of the coolest hip bumps, chest rolls, booty bounces and leg kicks…WOW you are going to feel like a backing dancer for your favourite singer!
 					</p>
 					<p>
+						Lose yourself in the music and take a moment to feel fantastic as you dance yourself fit. Once again we will raise your pulse with an easy to follow dance warm up and nip and tuck with a toning and strength section. Then the shoes are on and we are ready to learn the wild routine! Cue music, we are ready to rock out a sexy roar!
+					</p>
+					<p class="gold">
+						<i><b>Got moves? Get moves…make your shimmy a sexy shimmy!</b></i>
+					</p>
+				</div>
+			</div>
+
+			<hr/>
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="pink">The Bombshells<br/>Professional Burlesque Dancers</h3>
+					<p>
+						Bodybarre produces full length, and short version, aerial and ground-based shows. A unique Burlesque buzz for any occasion.
+					</p>
+					<p>
+						Book the Bodybarre Bombshells to entertain at your event and they will arrive armed and ready with twirling tassels, colourful costumes and sleek sensual dance routines.
+					</p>
+					<p>
+						Our dancers have appeared on TV, entertained at corporate events, and even danced the Can-Can on ice on the TV show "Torvill and Dean's Dancing On Ice"!
 						Fun loving dance packages are available for high-class tease and engaging excitement.
 					</p>
-				</div>
-				<div class="col-md-6">
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">Exercise Benefits</h3>
-							<ul class="ticked-list">
-								<li><span>Increase flexibility</span></li>
-								<li><span>Strengthen core</span></li>
-								<li><span>Firm your bum, abs, legs and arms</span></li>
-								<li><span>Feel good and energized</span></li>
-								<li><span>Improve balance and posture</span></li>
-								<li><span>Reduce stress levels</span></span></li>
-								<li><span>Strengthen heart with low cardio impact</span></span></li>
-							</ul>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">What to wear?</h3>
-							<p>
-								High Heeled shoes for Burlesque Dance. Please ensure your shoes have a strap, no sling backs.
-								Frilly knickers or leggings, corset or t-shirt, please wear whatever you feel comfortable in.
-								Bodybarre provide fun dress-up props such as hats, boas, gloves and fans.
-								Students may be required to supply their own props such as stockings, shirt or dress to use in class – nothing is required for week one of the course,
-								your instructor will inform you if an item is required.
-							</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="pink">When?</h3>
-							<?php echo getClassTimesFor("Burlesque"); ?>
-						</div>
-					</div>
+					<p>
+						Get in touch to discuss your requirements.
+					</p>
+					<p class="gold">
+						<i><b>A visual feast!</b></i>
+					</p>
 				</div>
 			</div>
-			<div class="row margined-row">
-				<div class="col-md-6">
-					<h3 class="pink">How to book?</h3>
-					<p>Book in advance only; limited places due to limited props.</p>
-					<p>To check availability and to book a place, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
-				</div>
-				<div class="col-md-6">
-					<h3 class="pink">How much?</h3>
-					<p>The four week burlesque course costs £35.00</p>
-					<p>Prices vary for burlesque show. E-mail <span class="gold">karen@bodybarre.co.uk</span> to discuss your requirements.</p>
-				</div>
-			</div>
+
 			<hr/>
+
 			<div class="row">
 				<div class="col-md-6">
-					<h3 class="pink">Professional burlesque dance troupe</h3>
-					<p>
-						Bodybarre produces full length and short version shows to bring a unique Burlesque buzz for any
-						occasion.
-						Book the Bodybarre Burlesquers to entertain at your event and they will arrive armed and ready with
-						twirling tassels, colourful costumes and sleek sensual dance routines.
-						Our dancers have appeared on TV, entertained at corporate events, and even danced the Can-Can on
-						ice on the TV show "Torvill and Dean's Dancing On Ice"!
-					</p>
-					<p>
-						Fun loving dance packages are available for high-class tease and engaging excitement.
-					</p>
+				<h3 class="pink">When are these classes?</h3>
+					<p>Please see our <a href="../timetable">timetable</a> for the latest class listings.</p>
 				</div>
 				<div class="col-md-6">
-					<h3 class="pink">How to book?</h3>
-					<p>Book in advance only.</p>
-					<p>To check availability and book, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
+				<h3 class="pink">How to book?</h3>
+					<p>Book in advance only; limited places due to limited equipment.</p>
+					<p>To check availability and to book a place, e-mail <span class="gold">karen@bodybarre.co.uk</span></p>
 				</div>
 			</div>
 
