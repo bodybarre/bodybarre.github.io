6a4b2c83acf8b1edf81b18f4c407eb1d3f4eb072
Benjamin Dale <ben@ben-dale.co.uk>
2018-05-05 16:34:41 +0100

    finish contact page

--- contact/index.php
+++ contact/index.php
@@ -28,22 +28,60 @@
 			</nav>
 
 			<div class="row">
-				<div class="col-md-12">
-					<div class="under-construction">
-						This page is currently under construction.
-						<br/><br/>
-						<i><span class="gold">Check back soon!</span></i>
+				<div class="col-xs-12">
+					<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+					<style type="text/css">
+						#mc_embed_signup{clear:left; font:14px; color: white; Helvetica,Arial,sans-serif; width:100%;}
+						/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+							We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+					</style>
+					<div id="mc_embed_signup">
+					<form action="https://bodybarre.us12.list-manage.com/subscribe/post?u=cf54f835a7c60e8a1ce01cb32&amp;id=d218b2efd7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<div id="mc_embed_signup_scroll">
+								<label for="mce-EMAIL">Subscribe to our mailing list!</label>
+								<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+								<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+								<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cf54f835a7c60e8a1ce01cb32_d218b2efd7" tabindex="-1" value=""></div>
+								<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+							</div>
+					</form>
 					</div>
 				</div>
 			</div>
-			<br/>
+
+			<hr/>
+
 			<div class="row">
-				<div style="text-align:center; font-size:16px;" class="col-md-12">
-					<p class="pink">
-						In the mean time, contact karen@bodybarre.co.uk for any and all queries!
+				<div class="col-md-6">
+					<h3 class="pink">Contact Us</h3>
+					<p>
+						Get in touch. Please email <span class="gold">karen@bodybarre.co.uk</span>
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h3 class="pink">Studio Address</h3>
+					<address>
+						<strong>Bodybarre Studio</strong><br/>
+						2nd Floor<br/>
+						35-37 Thomas Street<br/>
+						Northern Quarter<br/>
+						Manchester<br/>
+						M4 1NA<br/>
+					</address>
+				</div>
+			</div>
+
+			<hr/>
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="pink">Follow us on social media!</h3>
+					<p>
+						Please like Bodybarre on <a href="http://www.facebook.com/bodybarre">Facebook</a>, <a href="http://www.twitter.com/BodybarreUK">Twitter</a>, <a href="http://www.youtube.com/user/karenBODYBARRE">YouTube</a> and <a href="http://www.instagram.com/bodybarrekaz">Instagram</a> to see class photos, videos and news updates.
 					</p>
 				</div>
 			</div>
+
 <?php
 	include '../footer.php';
 ?>
